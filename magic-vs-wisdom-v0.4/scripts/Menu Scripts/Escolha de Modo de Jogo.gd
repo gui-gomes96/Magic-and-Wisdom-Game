99898135d8176a1d8b6973b954f96ad793a34556
gui-gomes96 <gelsongomes2020gel@gmail.com>
2025-10-22 07:10:55 -0300

    Add files via upload

---
+++ magic-vs-wisdom-v0.4/scripts/Menu Scripts/Escolha de Modo de Jogo.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_solo_pressed():
+	get_tree().change_scene_to_file("res://scenes/Menu Scenes/Escolha de Dificuldade.tscn")
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Menu Scenes/Menu.tscn")
+
+
+func _on_quiz_pressed():
+		get_tree().change_scene_to_file("res://scenes/Combate/QUIZ/Escolha de Materia Quiz.tscn")
