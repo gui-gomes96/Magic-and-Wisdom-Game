2bceb9de1c49fc85c5c9585775e725062b7bf0e1
gui-gomes96 <gelsongomes2020gel@gmail.com>
2025-10-27 14:57:05 -0300

    Delete magic-vs-wisdom-v0.2 directory

--- magic-vs-wisdom-v0.2/scripts/Menu Scripts/Escolha de Modo de Jogo.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_solo_pressed():
-	get_tree().change_scene_to_file("res://scenes/Menu Scenes/Escolha de Dificuldade.tscn")
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Menu Scenes/Menu.tscn")
